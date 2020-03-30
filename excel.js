dc5095a1698b518588e7bc18b8287e907e833710
gxf <243241838@qq.com>
2020-03-30 22:45:45 +0800

    上传

---
+++ excel.js
@@ -0,0 +1,63 @@
+import XLSX from 'xlsx';
+function sheet2blob(sheet, sheetName) {
+    sheetName = sheetName || 'sheet1';
+    var workbook = {
+        SheetNames: [sheetName],
+        Sheets: {}
+    };
+    workbook.Sheets[sheetName] = sheet; // 生成excel的配置项
+
+    var wopts = {
+        bookType: 'xlsx', // 要生成的文件类型
+        bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+        type: 'binary'
+    };
+    var wbout = XLSX.write(workbook, wopts);
+    var blob = new Blob([s2ab(wbout)], {
+        type: "application/octet-stream"
+    }); // 字符串转ArrayBuffer
+    return blob;
+}
+function s2ab(s) {
+    var buf = new ArrayBuffer(s.length);
+    var view = new Uint8Array(buf);
+    for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+}
+function openDownloadDialog(url, saveName) {
+    if (typeof url == 'object' && url instanceof Blob) {
+        url = URL.createObjectURL(url); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || '';
+    var event;
+    if (window.MouseEvent) event = new MouseEvent('click');
+    else {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+    aLink.dispatchEvent(event);
+}
+const downExcel = (url) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function (e) {
+        if (xhr.status == 200) {
+            var data = new Uint8Array(xhr.response)
+            var workbook = XLSX.read(data, { type: 'array' });
+            console.log(workbook)
+            openDownloadDialog(sheet2blob(workbook.Sheets[workbook.SheetNames]), workbook.SheetNames + '.xlsx');
+        }
+    };
+    xhr.send();
+}
+const downTable = (id, tableName) => {
+    var table1 = document.querySelector(id);
+    var sheet = XLSX.utils.table_to_sheet(table1);
+    openDownloadDialog(sheet2blob(sheet), tableName + '.xlsx');
+}
+export {
+    downExcel
+}
